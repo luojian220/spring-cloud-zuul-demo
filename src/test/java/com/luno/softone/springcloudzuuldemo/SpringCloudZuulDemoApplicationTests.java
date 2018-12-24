891b77ae6956eb27587dfdd74557513a9ca17a1f
罗剑 <luojian220@qq.com>
2018-12-24 15:34:11 +0800

    first commit

---
+++ src/test/java/com/luno/softone/springcloudzuuldemo/SpringCloudZuulDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.luno.softone.springcloudzuuldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudZuulDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
